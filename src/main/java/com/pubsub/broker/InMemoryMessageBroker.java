5b9ff5f9ce39cd46a36d4ab5304a7a444724784f
sausageRoll <drozdovigor1996@gmail.com>
2020-02-29 11:49:34 +0100

    Revert "commit + offset"
    This reverts commit 70bf56ca

--- src/main/java/com/pubsub/broker/InMemoryMessageBroker.java
+++ src/main/java/com/pubsub/broker/InMemoryMessageBroker.java
@@ -3,46 +3,42 @@ package com.pubsub.broker;
 import com.pubsub.model.Message;
 import java.util.Collection;
 import java.util.Map;
+import java.util.NoSuchElementException;
 import java.util.UUID;
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.TimeUnit;
 
 public class InMemoryMessageBroker implements MessageBroker {
 
-    private final Map<String, Integer> committedOffset = new ConcurrentHashMap<>();
+    private final Map<String, Integer> lastRead = new ConcurrentHashMap<>();
 
-    private final Map<String, CopyOnWriteArrayList<Message<String>>> topics = new ConcurrentHashMap<>();
+    private final Map<String, BlockingQueue<Message<String>>> topics = new ConcurrentHashMap<>();
 
     @Override
     public boolean createTopic(String topicName) {
-        return topics.putIfAbsent(topicName, new CopyOnWriteArrayList<>()) == null;
+        return topics.putIfAbsent(topicName, new LinkedBlockingQueue<>()) == null;
     }
 
     @Override
     public String subscribe(String topic) {
         String subscriberKey = UUID.randomUUID().toString();
-        committedOffset.put(topic + subscriberKey, 0);
+        lastRead.put(topic + subscriberKey, 0);
         return subscriberKey;
     }
 
     @Override
     public boolean unsubscribe(String topic, String subscriberKey) {
-        return committedOffset.remove(topic + subscriberKey) != null;
+        return lastRead.remove(topic + subscriberKey) != null;
     }
 
     @Override
     public void publishMessage(String topic, Message<String> message) {
         System.out.println(String.format("message %s came to topic %s", message.getValue(), topic));
-        topics.compute(topic, (t, q) -> {
-            if (q != null) {
-                q.add(message);
-                return q;
-            } else {
-                CopyOnWriteArrayList<Message<String>> res = new CopyOnWriteArrayList<>();
-                res.add(message);
-                return res;
-            }
+        topics.computeIfPresent(topic, (t, q) -> {
+            q.add(message);
+            return q;
         });
     }
 
@@ -53,20 +49,38 @@ public class InMemoryMessageBroker implements MessageBroker {
 
     @Override
     public Message<String> poll(String topic, String subscriberKey, int timeout, TimeUnit unit) {
-        Integer offset = committedOffset.get(topic + subscriberKey);
-        if (offset == null) {
-            return null;
-        }
-        if (topics.get(topic).size() > offset) {
-            return topics.get(topic).get(offset);
+        BlockingQueue<Message<String>> messages = topics.get(topic);
+
+        Integer state = lastRead.get(topic + subscriberKey);
+        if (state == 2) {
+            try {
+                return messages.poll(timeout, unit);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        } else if (state == 1) {
+            lastRead.computeIfPresent(topic + subscriberKey, (t, o) -> 2);
+            return pollTwice(messages);
+        } else if (state == 0) {
+            try {
+                Message<String> message = messages.element();
+                lastRead.computeIfPresent(topic + subscriberKey, (t, o) -> 1);
+                return message;
+            } catch (NoSuchElementException ex) {
+                return null;
+            }
         } else {
-            return null;
+            throw new IllegalStateException("state should be 0 or 1 or 2");
         }
     }
 
-    @Override
-    public boolean commitOffset(String topic, String subscriberKey) {
-        return committedOffset.computeIfPresent(topic + subscriberKey, (t, offset) -> offset + 1) != null;
+    private synchronized Message<String> pollTwice(BlockingQueue<Message<String>> queue) {
+        try {
+            queue.poll(0, TimeUnit.MILLISECONDS);
+            return queue.poll(0, TimeUnit.MILLISECONDS);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
