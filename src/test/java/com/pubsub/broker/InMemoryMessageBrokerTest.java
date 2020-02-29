765b485a2e2ced072fd44a4035e672e78ccc1617
sausageRoll <drozdovigor1996@gmail.com>
2020-02-29 11:49:22 +0100

    Revert "add tests"
    This reverts commit 02fb05bc

--- src/test/java/com/pubsub/broker/InMemoryMessageBrokerTest.java
+++ src/test/java/com/pubsub/broker/InMemoryMessageBrokerTest.java
@@ -74,12 +74,6 @@ class InMemoryMessageBrokerTest {
 
         messageBroker.commitOffset("topicTest", key);
         assertNull(messageBroker.poll("topicTest", key));
-
-        String key2 = messageBroker.subscribe("topicTest");
-        assertEquals(messageBroker.poll("topicTest", key).getValue(), "message");
-        messageBroker.commitOffset("topicTest", key2);
-        assertEquals(messageBroker.poll("topicTest", key).getValue(), "message1");
-
     }
 
 }
